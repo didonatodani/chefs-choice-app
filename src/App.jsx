1493f7480c12e278293ee09312cf2b9aed3194d0
Daniela Di Donato Vassallo <didonatodani@gmail.com>
2024-11-05 15:50:24 +0100

    sidebar files created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Footer from './components/Footer/Footer'
+import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
 
 
   return (
     <>
-
+    
+      <Sidebar/>
       <Footer/>
     </>
   )
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import "./Sidebar.css"
+
+function Sidebar(){
+    return(
+        <aside className="sidebar">
+            <p>hello hello</p>
+        </aside>
+    )
+}
+
+export default Sidebar
