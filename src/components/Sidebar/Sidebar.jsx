b05b0c1836f7249ace56866475181e102ce6c6a7
Daniela Di Donato Vassallo <didonatodani@gmail.com>
2024-11-05 16:19:50 +0100

    sidebar done

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,15 @@
 import "./Sidebar.css"
+import menuIcon from "../../assets/menu-icon.svg"
 
 function Sidebar(){
     return(
         <aside className="sidebar">
-            <p>hello hello</p>
+            <img src={menuIcon} alt="menu icon" id="menu-icon"/>
+            <ul>
+                <li>Home</li>
+                <li>Recipes</li>
+                <li>About</li>
+            </ul>
         </aside>
     )
 }
