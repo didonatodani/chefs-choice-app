570ed1481a8ebfae0e958d109933e3293e65ed17
Nigel Ferreres <nferrefe@gmail.com>
2024-11-10 19:18:00 +0100

    params solved

--- src/components/RecipesCard/RecipesCard.jsx
+++ src/components/RecipesCard/RecipesCard.jsx
@@ -1,33 +1,32 @@
 //no esta funcionando el useParams
-import { useParams } from "react-router-dom";
+//import { useParams } from "react-router-dom";
 
 //tot ok
-import recipesData from "../../data/recipesData.json";
+//import recipesData from "../../data/recipesData.json"; Dejo de importar esto de nuevo
 
-function RecipesCard() {
+function RecipesCard({ recipe: {image, calories, description, servings} }) {
+  //Añado un destructure
 
-//no está recibiendo el id:
-  const {recipeId} = useParams();
-console.log("This is the ID: ",recipeId); //Undefined
+  //no está recibiendo el id:
+  //const {recipeId} = useParams();
+  //console.log("This is the ID: ",recipeId); //Undefined
 
-  const recipeDetail = recipesData.find((recipe) => {
+  //console.log("This is teh ID: ", recipeId);
+
+  /*const recipeDetail = recipesData.find((recipe) => {
     // console.log(typeof recipe.id);
     // console.log(typeof recipeId)
     return recipe.id == recipeId
     ;
-  });
+  }); */
   //si cambiamoS recipeId por un id real(por ej "9e7f0d61-aa6b-418a-b56a-6418d19e6e1d", SÍ FUNCIONA EL RENDERIZADO)
-
-  if(!recipeDetail){
-    return <h1>Details not found</h1>
-  } //Cambio, borrar este codigo para dejarlo igual
 
   return (
     <div className="recipes-card">
-      <img src={recipeDetail.image} alt="" />
-      <h1>{recipeDetail.calories}</h1>
-      <p>{recipeDetail.description}</p>
-      <p>{recipeDetail.servings}</p>
+      <img src={image} alt="" />
+      <h1>{calories}</h1>
+      <p>{description}</p>
+      <p>{servings}</p>
       <div>
         <button>Details</button>
       </div>
--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,12 +1,23 @@
-import RecipesCard from "../components/RecipesCard/RecipesCard"
+import RecipesCard from "../components/RecipesCard/RecipesCard";
+import { useParams } from "react-router-dom";
+import recipesData from "../data/recipesData.json";
 
-function ItemDetails(){
-    //Se ha añadido el componente
-    return(
-        <>
-        <RecipesCard /> 
-        </>
-    )
+function ItemDetails() {
+  const { recipeId } = useParams();
+  console.log("This is the ID: ", recipeId);
+
+  const recipe = recipesData.find((recipe) => recipe.id === recipeId);
+
+  if (!recipe) {
+    return <h1>Details not found</h1>;
+  } //Cambio, borrar este codigo para dejarlo igual
+
+  //Se ha añadido el componente
+  return (
+    <>
+      <RecipesCard recipe={recipe} />
+    </>
+  );
 }
 
-export default ItemDetails
+export default ItemDetails;
