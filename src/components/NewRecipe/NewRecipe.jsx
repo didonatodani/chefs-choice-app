7366434cbcf408fc2a2a819e6b647ecab3457512
Daniela Di Donato Vassallo <didonatodani@gmail.com>
2024-11-11 17:58:38 +0100

    recipes styles

--- src/components/NewRecipe/NewRecipe.jsx
+++ src/components/NewRecipe/NewRecipe.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useId } from "react";
-
+import "./NewRecipe.css";
 
 function NewRecipe({ recipesArray, setRecipesArray }) {
   const [name, setName] = useState("");
@@ -13,50 +13,62 @@ function NewRecipe({ recipesArray, setRecipesArray }) {
   function handleSubmit(event) {
     event.preventDefault();
     const newRecipe = {
-      id: Math.floor(Math.random()* 999999),
+      id: Math.floor(Math.random() * 999999),
       name,
       calories,
       image,
       servings,
     };
 
-    console.log(newRecipe)
+    console.log(newRecipe);
 
     setRecipesArray([...recipesArray, newRecipe]);
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input style={{display: "none"}} id={id} />
-      <label htmlFor="name">Name:</label>
-      <input
-        type="text"
-        id="name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <label htmlFor="calories">Calories:</label>
-      <input
-        type="text"
-        id="calories"
-        value={calories}
-        onChange={(e) => setCalories(e.target.value)}
-      />
-      <label htmlFor="image">Image url:</label>
-      <input
-        type="text"
-        id="image"
-        value={image}
-        onChange={(e) => setImage(e.target.value)}
-      />
-      <label htmlFor="servings">Servings:</label>
-      <input
-        type="text"
-        id="servings"
-        value={servings}
-        onChange={(e) => setServings(e.target.value)}
-      />
-      <button type="submit">Add your recipe!</button>
+    <form onSubmit={handleSubmit} id="form">
+      <input style={{ display: "none" }} id={id} hidden />
+      <div className="form-div">
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className="form-div">
+        <label htmlFor="calories">Calories:</label>
+        <input
+          type="text"
+          id="calories"
+          value={calories}
+          onChange={(e) => setCalories(e.target.value)}
+        />
+      </div>
+      <div className="form-div">
+        <label htmlFor="image">Image url:</label>
+        <input
+          type="text"
+          id="image"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+        />
+      </div>
+      <div className="form-div">
+        <label htmlFor="servings">Servings:</label>
+        <input
+          type="text"
+          id="servings"
+          value={servings}
+          onChange={(e) => setServings(e.target.value)}
+        />
+      </div>
+      <div className="form-div">
+        <button type="submit" id="form-btn">
+          Add your recipe!
+        </button>
+      </div>
     </form>
   );
 }
